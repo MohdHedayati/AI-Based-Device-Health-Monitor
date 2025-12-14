e8d3a1a02558d6861ec827c0a9afd50d4187028f
Once-1296 <awwab.wadekar@gmail.com>
2025-12-14 18:51:54 +0530

    status check

--- scripts/start_data_coll.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/archuserbtw/AI-BASED-DEVICE-HEALTH-MONITOR
-venv/bin/python App.py
-cd /home/archuserbtw/AI-BASED-DEVICE-HEALTH-MONITOR/data
-../venv/bin/python -m http.server 8081
